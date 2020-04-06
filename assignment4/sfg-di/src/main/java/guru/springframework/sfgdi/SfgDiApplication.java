e0e17acba2eaa3e269ff8ee700b15fc42bf56c1a
Sumit Ray <sumitrayleo@gmail.com>
2020-04-06 12:08:26 +0530

    Assignment 4

---
+++ assignment4/sfg-di/src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -0,0 +1,20 @@
+package guru.springframework.sfgdi;
+
+import guru.springframework.sfgdi.controllers.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SfgDiApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+
+		PetController petController = ctx.getBean("petController", PetController.class);
+		System.out.println("--- The Best Pet is ---");
+		System.out.println(petController.whichPetIsTheBest());
+
+	}
+
+}
