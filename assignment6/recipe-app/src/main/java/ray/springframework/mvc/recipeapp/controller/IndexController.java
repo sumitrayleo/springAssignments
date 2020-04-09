3d9db2f57b481b95cffbbf242641d38d82944343
Sumit Ray <sumitrayleo@gmail.com>
2020-04-09 20:58:55 +0530

    Added index page

---
+++ assignment6/recipe-app/src/main/java/ray/springframework/mvc/recipeapp/controller/IndexController.java
@@ -0,0 +1,14 @@
+package ray.springframework.mvc.recipeapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "/index.html", "index"})
+    public String getIndexPage(){
+        return "index";
+    }
+
+}
