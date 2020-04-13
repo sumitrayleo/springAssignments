b657c43eebd4690871bc1e89c8e49fb3754cf7af
Sumit Ray <sumitrayleo@gmail.com>
2020-04-13 21:40:18 +0530

    Fix

--- assignment6/recipe-app/src/main/java/ray/springframework/mvc/recipeapp/bootstrap/RecipeDataLoader.java
+++ assignment6/recipe-app/src/main/java/ray/springframework/mvc/recipeapp/bootstrap/RecipeDataLoader.java
@@ -32,16 +32,16 @@ public class RecipeDataLoader implements CommandLineRunner {
 
     private void setupSpicyGrilledChicken() {
         final Recipe spicyChicken = new Recipe();
-        spicyChicken.getIngredients().add(getIngredient("dried cumin", BigDecimal.valueOf(2), "Teaspoon"));
-        spicyChicken.getIngredients().add(getIngredient("dried oregano", BigDecimal.valueOf(1), "Teaspoon"));
-        spicyChicken.getIngredients().add(getIngredient("sugar", BigDecimal.valueOf(1), "Teaspoon"));
-        spicyChicken.getIngredients().add(getIngredient("salt", BigDecimal.valueOf(.5), "Teaspoon"));
-        spicyChicken.getIngredients().add(getIngredient("ancho chili powder", BigDecimal.valueOf(2), "Tablespoon"));
-        spicyChicken.getIngredients().add(getIngredient("garlic", BigDecimal.valueOf(1), "Clove"));
-        spicyChicken.getIngredients().add(getIngredient("finely grated orange zest", BigDecimal.valueOf(1), "Tablespoon"));
-        spicyChicken.getIngredients().add(getIngredient("fresh-squeezed orange juice", BigDecimal.valueOf(3), "Tablespoon"));
-        spicyChicken.getIngredients().add(getIngredient("olive oil", BigDecimal.valueOf(2), "Tablespoon"));
-        spicyChicken.getIngredients().add(getIngredient("boneless chicken thighs", BigDecimal.valueOf(11.25), "Pound"));
+        spicyChicken.getIngredients().add(getIngredient("dried cumin", BigDecimal.valueOf(2), "Teaspoon", spicyChicken));
+        spicyChicken.getIngredients().add(getIngredient("dried oregano", BigDecimal.valueOf(1), "Teaspoon", spicyChicken));
+        spicyChicken.getIngredients().add(getIngredient("sugar", BigDecimal.valueOf(1), "Teaspoon", spicyChicken));
+        spicyChicken.getIngredients().add(getIngredient("salt", BigDecimal.valueOf(.5), "Teaspoon", spicyChicken));
+        spicyChicken.getIngredients().add(getIngredient("ancho chili powder", BigDecimal.valueOf(2), "Tablespoon", spicyChicken));
+        spicyChicken.getIngredients().add(getIngredient("garlic", BigDecimal.valueOf(1), "Clove", spicyChicken));
+        spicyChicken.getIngredients().add(getIngredient("finely grated orange zest", BigDecimal.valueOf(1), "Tablespoon", spicyChicken));
+        spicyChicken.getIngredients().add(getIngredient("fresh-squeezed orange juice", BigDecimal.valueOf(3), "Tablespoon", spicyChicken));
+        spicyChicken.getIngredients().add(getIngredient("olive oil", BigDecimal.valueOf(2), "Tablespoon", spicyChicken));
+        spicyChicken.getIngredients().add(getIngredient("boneless chicken thighs", BigDecimal.valueOf(11.25), "Pound", spicyChicken));
 
         spicyChicken.getCategories().add(categoryRepository.findByDescription("American").get());
 
@@ -57,17 +57,16 @@ public class RecipeDataLoader implements CommandLineRunner {
 
     private void setupPerfectGuacamole() {
         final Recipe guacamole = new Recipe();
-        guacamole.getIngredients().add(getIngredient("ripe avocados", BigDecimal.valueOf(2), "Number"));
-        guacamole.getIngredients().add(getIngredient("salt", BigDecimal.valueOf(.25), "Teaspoon"));
-        guacamole.getIngredients().add(getIngredient("fresh lime juice", BigDecimal.valueOf(1), "Tablespoon"));
-        guacamole.getIngredients().add(getIngredient("minced red onion", BigDecimal.valueOf(.25), "Cup"));
-        guacamole.getIngredients().add(getIngredient("finely chopped cilantro", BigDecimal.valueOf(2), "Tablespoon"));
-        guacamole.getIngredients().add(getIngredient("freshly grated black pepper", BigDecimal.valueOf(1), "Dash"));
-        guacamole.getIngredients().add(getIngredient("ripe tomato", BigDecimal.valueOf(.5), "Number"));
-        guacamole.getIngredients().add(getIngredient("Red radishes", BigDecimal.valueOf(2), "Number"));
-        guacamole.getIngredients().add(getIngredient("Tortilla chips", BigDecimal.valueOf(1), "Packet"));
+        guacamole.getIngredients().add(getIngredient("ripe avocados", BigDecimal.valueOf(2), "Number", guacamole));
+        guacamole.getIngredients().add(getIngredient("salt", BigDecimal.valueOf(.25), "Teaspoon", guacamole));
+        guacamole.getIngredients().add(getIngredient("fresh lime juice", BigDecimal.valueOf(1), "Tablespoon", guacamole));
+        guacamole.getIngredients().add(getIngredient("minced red onion", BigDecimal.valueOf(.25), "Cup", guacamole));
+        guacamole.getIngredients().add(getIngredient("finely chopped cilantro", BigDecimal.valueOf(2), "Tablespoon", guacamole));
+        guacamole.getIngredients().add(getIngredient("freshly grated black pepper", BigDecimal.valueOf(1), "Dash", guacamole));
+        guacamole.getIngredients().add(getIngredient("ripe tomato", BigDecimal.valueOf(.5), "Number", guacamole));
+        guacamole.getIngredients().add(getIngredient("Red radishes", BigDecimal.valueOf(2), "Number", guacamole));
+        guacamole.getIngredients().add(getIngredient("Tortilla chips", BigDecimal.valueOf(1), "Packet", guacamole));
 
-        System.out.println("### Category = " + categoryRepository.findByDescription("Mexican"));
         guacamole.getCategories().add(categoryRepository.findByDescription("Mexican").get());
 
         guacamole.setUrl("https://www.simplyrecipes.com/recipes/perfect_guacamole/");
@@ -80,11 +79,12 @@ public class RecipeDataLoader implements CommandLineRunner {
         recipeRepository.save(guacamole);
     }
 
-    private Ingredient getIngredient(String description, BigDecimal amount, String uom) {
+    private Ingredient getIngredient(String description, BigDecimal amount, String uom, Recipe recipe) {
         Ingredient ingredient = new Ingredient();
         ingredient.setDescription(description);
         ingredient.setAmount(amount);
         ingredient.setUom(unitOfMeasureRepository.findByDescription(uom).get());
+        ingredient.setRecipe(recipe);
         return ingredient;
     }
 }
