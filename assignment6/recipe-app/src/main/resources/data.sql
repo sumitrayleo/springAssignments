f8100be8be67ffd1af267163a601966d6a5cdfbf
Sumit Ray <sumitrayleo@gmail.com>
2020-04-13 19:51:37 +0530

    data.sql added to insert tet data

---
+++ assignment6/recipe-app/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO Category (description) VALUES ('American');
+INSERT INTO Category (description) VALUES ('Italian');
+INSERT INTO Category (description) VALUES ('Mexican');
+INSERT INTO Category (description) VALUES ('Fast Food');
+
+INSERT INTO unit_of_measure (description) VALUES ('Teaspoon');
+INSERT INTO unit_of_measure (description) VALUES ('Tablespoon');
+INSERT INTO unit_of_measure (description) VALUES ('Cup');
+INSERT INTO unit_of_measure (description) VALUES ('Pinch');
+INSERT INTO unit_of_measure (description) VALUES ('Ounce');
+
