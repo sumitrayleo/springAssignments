da1a140db2d447faa390d199983cd914ad397031
Sumit Ray <sumitrayleo@gmail.com>
2020-04-15 14:47:55 +0530

    Capture Arguments

--- assignment6/recipe-app/src/test/java/ray/springframework/mvc/recipeapp/controller/RecipeControllerTest.java
+++ assignment6/recipe-app/src/test/java/ray/springframework/mvc/recipeapp/controller/RecipeControllerTest.java
@@ -2,10 +2,15 @@ package ray.springframework.mvc.recipeapp.controller;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
+import ray.springframework.mvc.recipeapp.domain.Recipe;
 import ray.springframework.mvc.recipeapp.service.RecipeService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
@@ -28,9 +33,29 @@ class RecipeControllerTest {
 
     @Test
     void getAllRecipes() {
-        assertEquals("recipe/list", recipeController.getAllRecipes(model));
+        // given
+        Recipe recipe1 = new Recipe();
+        recipe1.setId(1L);
+        Recipe recipe2 = new Recipe();
+        recipe2.setId(1L);
 
+        List<Recipe> recipes = new ArrayList<>();
+        recipes.add(recipe1);
+        recipes.add(recipe2);
+
+        // when
+        when(recipeService.findAll()).thenReturn(recipes);
+        String returnVal = recipeController.getAllRecipes(model);
+        ArgumentCaptor<List<Recipe>> argumentCaptor = ArgumentCaptor.forClass(List.class);
+
+        // then
+        assertEquals("recipe/list", returnVal);
         verify(recipeService, times(1)).findAll();
-        verify(model, times(1)).addAttribute(eq("recipes"), anyList());
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+
+        List<Recipe> listOfRecipes = argumentCaptor.getValue();
+        assertEquals(2, listOfRecipes.size());
+
+
     }
 }
