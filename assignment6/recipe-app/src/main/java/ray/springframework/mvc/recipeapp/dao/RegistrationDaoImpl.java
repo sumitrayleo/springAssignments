b0bc420928ae95799e090740f438d01fe5114536
Sumit Ray <sumitrayleo@gmail.com>
2021-10-04 19:27:50 -0500

    Re-arranged code

--- assignment6/recipe-app/src/main/java/ray/springframework/mvc/recipeapp/dao/RegistrationDaoImpl.java
+++ assignment6/recipe-app/src/main/java/ray/springframework/mvc/recipeapp/dao/RegistrationDaoImpl.java
@@ -46,6 +46,24 @@ public class RegistrationDaoImpl implements RegistrationDao {
     @Override
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     public AppUser save(AppUser appUser, Set<Device> devices) {
+        final AppUser savedAppUserFromDB = appUserRepository.findByUserIdAndApp(appUser.getUserId(), appUser.getApp());
+
+        AppUser savedAppUser = null;
+        if(savedAppUserFromDB == null) {
+            // Create
+            savedAppUser = createRegistration(appUser, devices);
+        } else {
+            // Update
+            savedAppUser = updateRegistration(savedAppUserFromDB, devices);
+        }
+
+        if(savedAppUser == null) {
+            // TODO: throw some exception
+        }
+        return appUserRepository.save(savedAppUser);
+    }
+
+    private AppUser createRegistration(AppUser appUser, Set<Device> devices) {
         Set<AppUserDevice> appUserDevices = new HashSet<>();
         AppUser savedAppUser = appUserRepository.save(appUser);
 
@@ -62,6 +80,35 @@ public class RegistrationDaoImpl implements RegistrationDao {
             appUserDevices.add(appUserDevice);
         }
         savedAppUser.setAppUserDevices(appUserDevices);
-        return appUserRepository.save(appUser);
+        return appUserRepository.save(savedAppUser);
+    }
+
+    private AppUser updateRegistration(AppUser savedAppUserfromDB, Set<Device> devices) {
+        Set<AppUserDevice> appUserDevices = new HashSet<>();
+        for(Device device : devices) {
+            Device savedDeviceFromDB = deviceRepository.findByToken(device.getToken());
+            if(savedDeviceFromDB == null) {
+                // Create Device
+                savedDeviceFromDB = new Device();
+                savedDeviceFromDB.setToken(device.getToken());
+                savedDeviceFromDB.setPlatform(device.getPlatform());
+                savedDeviceFromDB.setActive(device.getActive());
+            }
+            savedDeviceFromDB.setName(device.getName());
+            final Device updatedDeviceFromDB = deviceRepository.save(savedDeviceFromDB);
+
+            AppUserDevice appUserAndDeviceFromDB = appUserDeviceRepository.findByAppUserAndDevice(savedAppUserfromDB, updatedDeviceFromDB);
+            if(appUserAndDeviceFromDB == null) {
+                appUserAndDeviceFromDB = new AppUserDevice();
+                appUserAndDeviceFromDB.setAppUser(savedAppUserfromDB);
+                appUserAndDeviceFromDB.setDevice(updatedDeviceFromDB);
+            }
+
+            // TODO: Need to see how to propagate this from DTO
+            appUserAndDeviceFromDB.setOptIn(false);
+            appUserDevices.add(appUserAndDeviceFromDB);
+        }
+        savedAppUserfromDB.setAppUserDevices(appUserDevices);
+        return appUserRepository.save(savedAppUserfromDB);
     }
 }
--- assignment6/recipe-app/src/main/java/ray/springframework/mvc/recipeapp/repository/AppUserDeviceRepository.java
+++ assignment6/recipe-app/src/main/java/ray/springframework/mvc/recipeapp/repository/AppUserDeviceRepository.java
@@ -2,10 +2,12 @@ package ray.springframework.mvc.recipeapp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import ray.springframework.mvc.recipeapp.domain.AppUser;
 import ray.springframework.mvc.recipeapp.domain.AppUserDevice;
 import ray.springframework.mvc.recipeapp.domain.AppUserDevicePK;
+import ray.springframework.mvc.recipeapp.domain.Device;
 
 @Repository
 public interface AppUserDeviceRepository extends JpaRepository<AppUserDevice, AppUserDevicePK> {
-
+    AppUserDevice findByAppUserAndDevice(AppUser appUser, Device device);
 }
